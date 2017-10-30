c32ee86d43a64b898beed698c620e469d3be6539
Joaquin <jalonsop12@iesvjp.es>
2017-10-30 18:07:27 +0100

    cambios varios

--- src/ControlJuego.java
+++ src/ControlJuego.java
@@ -35,11 +35,50 @@ public class ControlJuego {
 	 * 			El resto de posiciones que no son minas guardan en el entero cuántas minas hay alrededor de la celda
 	 */
 	public void inicializarPartida(){
-
-		
+		Random rd=new Random();
+		int[]x=new int[MINAS_INICIALES];//posicion x de las minas
+		int[]y=new int[MINAS_INICIALES];//posici�n y de las minas
+		/*
+		 * Le damos posiciones aleatorias a las minas
+		 */
+		for (int i = 0; i < x.length; i++) {
+			x[i]=rd.nextInt(LADO_TABLERO);
+			y[i]=rd.nextInt(LADO_TABLERO);
+		}
+		/**
+		 * si la posici�n de una mina coincide con otra se cambia la segunda
+		 */
+		for (int i = 0; i < x.length; i++) {
+			for (int j = 0; j < y.length; j++) {
+				if((x[i]==x[j])&&(y[i]==y[j])) {
+					x[j]=rd.nextInt(LADO_TABLERO);
+					y[j]=rd.nextInt(LADO_TABLERO);
+				}
+			}
+		}
+		/**
+		 * Pintamos las minas en el tablero
+		 */
+		for (int i = 0; i < y.length; i++) {
+			tablero[x[i]][y[i]]=MINA;
+		}
+		/**
+		 * Recorremos el tablero para calcular las minas adjuntas
+		 */
+		for (int i = 0; i < tablero.length; i++) {
+			for (int j = 0; j < tablero[i].length; j++) {
+				/*
+				 * Si la posici�n no es una mina
+				 * Se le asigna el valor de las minas alrededor
+				 */
+				if(tablero[i][j]!=MINA) {
+					tablero[i][j]=calculoMinasAdjuntas(i, j);
+				}
+			}
+		}
 	}
 	
-	/**Cálculo de las minas adjuntas:
+	/**C�lculo de las minas adjuntas:
 	 * Para calcular el número de minas tenemos que tener en cuenta que no nos salimos nunca del tablero.
 	 * Por lo tanto, como mucho la i y la j valdrán LADO_TABLERO-1.
 	 * Por lo tanto, como mucho la i y la j valdrán como poco 0.
@@ -48,11 +87,36 @@ public class ControlJuego {
 	 * @return : El número de minas que hay alrededor de la casilla [i][j]
 	 **/
 	private int calculoMinasAdjuntas(int i, int j){
-
+		int minasAlrededor=0;
+		if(i==0) {
+			//esta a la izquierda
+			if(j==0) {
+				//aqui estaria en la esquina superior izquierda
+			}else {
+				if(j==LADO_TABLERO-1) {
+					//esquina inferior izquierda
+				}else {
+					//j no es ni arriba ni abajo
+				}
+			}
+		}else {
+			if(i==LADO_TABLERO-1) {
+				//esta a la derecha
+				if(j==0) {
+					//aqui estaria en la esquina superior derecha
+				}else {
+					if(j==LADO_TABLERO-1) {
+						//esquina inferior derecha
+					}else {
+						//j no es ni arriba ni abajo
+					}
+			}
+		}
+		return minasAlrededor;
 	}
 	
 	/**
-	 * Método que nos permite 
+	 * M�todo que nos permite 
 	 * @pre : La casilla nunca debe haber sido abierta antes, no es controlado por el GestorJuego. Por lo tanto siempre sumaremos puntos
 	 * @param i: posición verticalmente de la casilla a abrir
 	 * @param j: posición horizontalmente de la casilla a abrir
